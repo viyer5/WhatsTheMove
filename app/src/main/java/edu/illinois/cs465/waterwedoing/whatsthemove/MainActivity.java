9e72ba25324613f9b4a8b94989f82256c00228ad
viyer5 <viyer5@buffalo.edu>
2018-11-22 02:56:03 -0600

    Barebones welcome page has been setup

--- app/src/main/java/edu/illinois/cs465/waterwedoing/whatsthemove/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/waterwedoing/whatsthemove/MainActivity.java
@@ -1,13 +1,30 @@
 package edu.illinois.cs465.waterwedoing.whatsthemove;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
+    private Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        next = (Button) findViewById(R.id.buttonNext);
+        next.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.buttonNext){
+            Intent intent = new Intent(this, AppHomePageActivity.class);
+            startActivity(intent);
+            startActivity(intent);
+        }
     }
 }

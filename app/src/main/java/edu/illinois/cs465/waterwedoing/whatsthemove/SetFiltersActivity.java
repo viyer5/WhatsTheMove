551ae642c918a8f5adff13bdc9cdc6aeef3283d1
viyer5 <viyer5@buffalo.edu>
2018-11-26 21:59:25 -0600

    FiltersActivity initiated

---
+++ app/src/main/java/edu/illinois/cs465/waterwedoing/whatsthemove/SetFiltersActivity.java
@@ -0,0 +1,13 @@
+package edu.illinois.cs465.waterwedoing.whatsthemove;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SetFiltersActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_filters);
+    }
+}

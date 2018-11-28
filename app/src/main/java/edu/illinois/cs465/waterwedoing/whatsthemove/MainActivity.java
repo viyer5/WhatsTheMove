853bd0e4ba4de3131dbc1919294a85cb62b17fe3
viyer5 <viyer5@buffalo.edu>
2018-11-28 17:25:19 -0600

    Main Activity unnecessary buttons removed

--- app/src/main/java/edu/illinois/cs465/waterwedoing/whatsthemove/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/waterwedoing/whatsthemove/MainActivity.java
@@ -9,32 +9,32 @@ import android.widget.Button;
 
 public class MainActivity extends Activity implements OnClickListener{
 
-    private Button next;
-    private Button slide;
+/*    private Button next;
+    private Button slide;*/
     private Button filters;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        next = (Button) findViewById(R.id.buttonNext);
-        slide = (Button) findViewById(R.id.buttonSlide);
+   /*     next = (Button) findViewById(R.id.buttonNext);
+        slide = (Button) findViewById(R.id.buttonSlide);*/
         filters = (Button) findViewById(R.id.FiltersPage);
         filters.setOnClickListener(this);
-        next.setOnClickListener(this);
-        slide.setOnClickListener(this);
+      /*  next.setOnClickListener(this);
+        slide.setOnClickListener(this);*/
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.buttonNext){
+ /*       if(v.getId() == R.id.buttonNext){
             Intent intent = new Intent(this, MapsActivity.class);
             startActivity(intent);
         }
         if(v.getId() == R.id.buttonSlide){
             Intent intent = new Intent(this, ScreenSlidePagerActivity.class);
             startActivity(intent);
-        }
+        }*/
         if(v.getId() == R.id.FiltersPage){
             Intent intent = new Intent(this, SetFiltersActivity.class);
             startActivity(intent);

1f85413cafe261ffc1e482cd595b9d98e71fc2b5
viyer5 <viyer5@buffalo.edu>
2018-11-28 02:26:53 -0600

    changes committed

--- app/src/main/java/edu/illinois/cs465/waterwedoing/whatsthemove/SetFiltersActivity.java
+++ app/src/main/java/edu/illinois/cs465/waterwedoing/whatsthemove/SetFiltersActivity.java
@@ -1,13 +1,43 @@
 package edu.illinois.cs465.waterwedoing.whatsthemove;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SetFiltersActivity extends Activity {
+public class SetFiltersActivity extends Activity implements View.OnClickListener {
+    private Button whatsTheMove;
+    private Button cancel;
+    private Button resetFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_filters);
+
+        whatsTheMove = (Button) findViewById(R.id.whatsTheMoveButton);
+        cancel = (Button) findViewById(R.id.cancelButton);
+        whatsTheMove.setOnClickListener(this);
+        cancel.setOnClickListener(this);
+        resetFilter = (Button) findViewById(R.id.resetFilter);
+        resetFilter.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.cancelButton){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        if(v.getId() == R.id.whatsTheMoveButton){
+            Intent intent = new Intent(this, ScreenSlidePagerActivity.class);
+            startActivity(intent);
+        }
+        if(v.getId() == R.id.resetFilter){
+            Intent intent = new Intent(this, SetFiltersActivity.class);
+            startActivity(intent);
+        }
     }
 }

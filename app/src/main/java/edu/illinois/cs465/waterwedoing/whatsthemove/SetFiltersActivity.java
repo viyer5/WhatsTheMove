cbc4d6fc5ef9436ea1f1cbf1c46807b9014c2654
viyer5 <viyer5@buffalo.edu>
2018-11-28 13:31:51 -0600

    Recommiting to avoid merge conflicts

--- app/src/main/java/edu/illinois/cs465/waterwedoing/whatsthemove/SetFiltersActivity.java
+++ app/src/main/java/edu/illinois/cs465/waterwedoing/whatsthemove/SetFiltersActivity.java
@@ -3,13 +3,23 @@ package edu.illinois.cs465.waterwedoing.whatsthemove;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class SetFiltersActivity extends Activity implements View.OnClickListener {
     private Button whatsTheMove;
     private Button cancel;
     private Button resetFilter;
+    private SeekBar priceRange;
+    private SeekBar distance;
+    private SeekBar duration;
+    private RadioButton pRangeAnyButton;
+    private RadioButton distanceRangeAnyButton;
+    private RadioButton durationRangeAnyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,75 @@ public class SetFiltersActivity extends Activity implements View.OnClickListener
         cancel.setOnClickListener(this);
         resetFilter = (Button) findViewById(R.id.resetFilter);
         resetFilter.setOnClickListener(this);
+        pRangeAnyButton = (RadioButton) findViewById(R.id.RadioButtonPriceRange);
+
+
+        priceRange = (SeekBar) findViewById(R.id.seekaBarPriceRange);
+        priceRange.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressVal = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressVal = progress;
+                pRangeAnyButton.setChecked(false);
+                Toast.makeText(getApplicationContext(), "Set a value between 0 to 100$", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                String output = "Price Range is Set To: " + progressVal + " $";
+                Toast.makeText(getApplicationContext(), output, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        distanceRangeAnyButton = (RadioButton) findViewById(R.id.RadioButtonDistanceRange);
+        distance = (SeekBar) findViewById(R.id.seekaBarDistance);
+        distance.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressVal = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressVal = progress;
+                distanceRangeAnyButton.setChecked(false);
+                Toast.makeText(getApplicationContext(), "Set a value between 0 mi to 30 mi", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                String output = "Distance Range is Set To: " + progressVal + " miles";
+                Toast.makeText(getApplicationContext(), output, Toast.LENGTH_LONG).show();
+            }
+        });
+        durationRangeAnyButton = (RadioButton) findViewById(R.id.RadioButtonDurationRange);
+        duration = (SeekBar)findViewById(R.id.seekaBarDuration);
+        duration.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressVal = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressVal = progress;
+                durationRangeAnyButton.setChecked(false);
+                Toast.makeText(getApplicationContext(), "Set a value between 0 hour to 12 hours", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                String output = "Duration Range is Set To: " + progressVal + " hours";
+                Toast.makeText(getApplicationContext(), output, Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 

0626edfc6018cacec5515aac75c3133b1b91c246
Aayush Gupta <aayush98@gmail.com>
2018-11-27 21:11:36 -0600

    Created hardcoded card layouts

--- app/src/main/java/edu/illinois/cs465/waterwedoing/whatsthemove/ScreenSlidePageFragment.java
+++ app/src/main/java/edu/illinois/cs465/waterwedoing/whatsthemove/ScreenSlidePageFragment.java
@@ -12,14 +12,17 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class ScreenSlidePageFragment extends Fragment {
 
 //    private int String_idx = 0;
-    private String titleString;
-    private String descString;
+    private String titleString = "Title";
+    private String descString = "Description Description Description";
     private boolean isFirst = false;
     private boolean isLast = false;
-    private int rating;
+    private int rating = 5;
+    private int timing = 1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState)
     {
@@ -32,6 +35,12 @@ public class ScreenSlidePageFragment extends Fragment {
         TextView actTitle = (TextView)rootView.findViewById(R.id.Activity_Title);
         actTitle.setText(titleString);
 
+        TextView actDesc = (TextView)rootView.findViewById(R.id.Activity_Description);
+        actDesc.setText(descString);
+
+        TextView actTime = (TextView)rootView.findViewById(R.id.Activity_Timing);
+        actTime.setText("This activity will take " + timing + " hour(s)");
+
         if (isFirst) {
             ImageView sLeft = (ImageView)rootView.findViewById(R.id.swipe_left);
             sLeft.setVisibility(View.INVISIBLE);
@@ -42,8 +51,28 @@ public class ScreenSlidePageFragment extends Fragment {
             sRight.setVisibility(View.INVISIBLE);
         }
 
-        ImageView ratingImg = (ImageView)rootView.findViewById(R.id.Activity_Rating);
+        ImageView activityImg = (ImageView)rootView.findViewById(R.id.Activity_Image);
         Resources res = getResources();
+        switch(titleString){
+            case "Krannert Art Museum":
+                activityImg.setImageDrawable(res.getDrawable(R.drawable.krannert));
+                break;
+            case "UI Ice Arena":
+                activityImg.setImageDrawable(res.getDrawable(R.drawable.ice_arena));
+                break;
+            case "Unviersity of Illinois Observatory":
+                activityImg.setImageDrawable(res.getDrawable(R.drawable.uofiobservatory));
+                break;
+            case "Cravings":
+                activityImg.setImageDrawable(res.getDrawable(R.drawable.cravings));
+                break;
+            case "Taco Bell":
+                activityImg.setImageDrawable(res.getDrawable(R.drawable.tacobell));
+                break;
+        }
+
+        ImageView ratingImg = (ImageView)rootView.findViewById(R.id.Activity_Rating);
+        res = getResources();
         switch (rating){
             case 1:
                 ratingImg.setImageDrawable(res.getDrawable(R.drawable.stars_1));
@@ -67,8 +96,12 @@ public class ScreenSlidePageFragment extends Fragment {
         return rootView;
     }
 
-    public void setString(String str) {
+    public void setTitle(String str) {
         titleString = str;
+    }
+
+    public void setDesc(String str) {
+        descString = str;
     }
 
     public void setFirst(boolean first) {
@@ -81,5 +114,9 @@ public class ScreenSlidePageFragment extends Fragment {
 
     public void setRating(int rating) {
         this.rating = rating;
+    }
+
+    public void setTiming(int timing) {
+        this.timing = timing;
     }
 }
